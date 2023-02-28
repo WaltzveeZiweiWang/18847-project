57d9a98b03fecdc45bbd0c5ca566f0e634faad8d
Ziwei Wang <96067195+WaltzveeZiweiWang@users.noreply.github.com>
2023-02-27 21:32:40 -0500

    Update worker-setup.sh

--- worker-setup.sh
+++ worker-setup.sh
@@ -22,11 +22,16 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce -y
+sudo apt-get install docker-ce-cli -y
+sudo apt-get install containerd.io -y
+sudo apt-get install docker-buildx-plugin -y 
+sudo apt-get install docker-compose-plugin -y
 
 sudo docker run hello-world
 
 sudo apt-get update
+
 sudo apt-get install docker-compose-plugin
 
 sudo apt-get install docker-compose-plugin
@@ -38,9 +43,5 @@ sudo apt-get install luarocks
 sudo luarocks install luasocket
 
 sudo dockerd &> /dev/null &
-
-git clone https://github.com/delimitrou/DeathStarBench.git
-
-cd DeathStarBench/socialNetwork/
 
 sudo docker swarm join --token SWMTKN-1-56q4l18gusq8zhyoti5mb3zdzts4l2frhdmcs0rto4at7ti4o4-deshp4lgnc40hyu3dka5yf3jz 128.110.216.30:2377
